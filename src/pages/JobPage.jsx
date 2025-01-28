d92d4fe8b0c2c873699a04a9a8554bc39c304f7e
Iagokoch <iagon.koch@gmail.com>
2025-01-28 16:27:29 -0300

    custom pag do trabalho solo

---
+++ src/pages/JobPage.jsx
@@ -0,0 +1,109 @@
+import { useParams, useLoaderData } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { FaMapMarker } from "react-icons/fa";
+
+const JobPage = () => {
+  const { id } = useParams();
+  const job = useLoaderData();
+
+  return (
+    <>
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            to="/jobs"
+            className="text-red-500 hover:text-red-700 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Job Listings
+          </Link>
+        </div>
+      </section>
+
+      <section className="bg-red-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <main>
+              <div className="bg-black p-6 rounded-lg shadow-md text-center md:text-left">
+                <div className="text-gray-400 mb-4">{job.type}</div>
+                <h1 className=" text-white text-3xl font-bold mb-4">
+                  {job.title}
+                </h1>
+                <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+                  <FaMapMarker className="text-red-700 mr-1" />
+                  <p className="text-red-700">{job.location}</p>
+                </div>
+              </div>
+
+              <div className="bg-black p-6 rounded-lg shadow-md mt-6">
+                <h3 className="text-red-700 text-lg font-bold mb-6">
+                  Job Description
+                </h3>
+
+                <p className=" text-white mb-4">{job.description}</p>
+
+                <h3 className="text-red-700 text-lg font-bold mb-2">Salary</h3>
+
+                <p className=" text-white mb-4">{job.salary} / Year</p>
+              </div>
+            </main>
+
+            {/* <!-- Sidebar --> */}
+            <aside>
+              <div className="bg-black p-6 rounded-lg shadow-md">
+                <h3 className="text-xl text-white font-bold mb-6">
+                  Company Info
+                </h3>
+
+                <h2 className=" text-white text-2xl">{job.company.name}</h2>
+
+                <p className=" text-white my-2">{job.company.description}</p>
+
+                <hr className="my-4" />
+
+                <h3 className=" text-white text-xl">Contact Email:</h3>
+
+                <p className="my-2 bg-red-100 p-2 font-bold rounded-xl">
+                  {job.company.contactEmail}
+                </p>
+
+                <h3 className=" text-white text-xl">Contact Phone:</h3>
+
+                <p className="my-2 bg-red-100 p-2 font-bold rounded-xl">
+                  {" "}
+                  {job.company.contactPhone}
+                </p>
+              </div>
+
+              <div className="bg-black p-6 rounded-lg shadow-md mt-6">
+                <h3 className="text-xl text-white font-bold mb-6">
+                  Manage Job
+                </h3>
+                <Link
+                  to={`/edit-job/${job.id}`}
+                  className="bg-emerald-600 hover:bg-emerald-700 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
+                  Edit Job
+                </Link>
+                <button
+                  onClick={() => onDeleteClick(job.id)}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
+                  Delete Job
+                </button>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
+  const data = await res.json();
+  return data;
+};
+
+export { JobPage as default, jobLoader };
