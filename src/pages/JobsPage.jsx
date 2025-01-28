b044af6fad0fa14553f20548ff83ccd9ef766c9b
Iagokoch <iagon.koch@gmail.com>
2025-01-28 14:39:01 -0300

    parte do proxy

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -2,7 +2,7 @@ import JobsListings from "../components/JobsListings";
 
 const JobsPage = () => {
   return (
-    <section className="bg-red-100 px-4 py-6">
+    <section className="bg-black px-4 py-6">
       <JobsListings />
     </section>
   );
